0238ae35f4f20f031348b97d22d40f1f75ac32e6
1404sania-begum <1404sania-begum@gmail.com>
2024-01-10 22:44:59 -0800

    initial commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function greet(name){
+    console.log(name  +" ",  "is a very Good Morning" );
+
+}
+let name0 = "Sania Begum";
+let name1 = "Samreen Begum";
+let name2 = "junkook";
+let name3 ="park jimin";
+greet (name0);
+greet (name1);
+greet (name2);
+greet (name3);
